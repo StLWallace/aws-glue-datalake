a548a3ecb7285e34d16ff4845f168c6ad384ba9b
Stephen Wallace <stleewallace@gmail.com>
2023-10-25 22:13:29 -0500

    Fixed some stuff

--- terraform/glue-jobs.tf
+++ terraform/glue-jobs.tf
@@ -7,19 +7,18 @@ locals {
 resource "aws_s3_object" "glue_job_scripts" {
   for_each = fileset("../glue/jobs", "*.py")
   bucket   = aws_s3_bucket.glue_scripts.id
-  key      = "jobs/"
+  key      = "jobs/${each.value}"
   source   = "../glue/jobs/${each.value}"
 
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("../glue/jobs/${each.value}")
 }
 
 
 resource "aws_glue_job" "test_job" {
-  name     = "test-job-${var.environment}"
-  role_arn = aws_iam_role.glue.arn
+  name              = "test-job-${var.environment}"
+  role_arn          = aws_iam_role.glue.arn
+  number_of_workers = 2
+  worker_type       = "G.1X"
 
   glue_version = "4.0"
   command {
